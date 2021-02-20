553a1d113ba0ca7a5812e07e925b14734778b29d
Costalesjonatan <42694800+Costalesjonatan@users.noreply.github.com>
2021-02-20 11:11:03 -0300

    Se agregan dependencia y se completa la clase usuario

--- src/main/java/com/proyecto/practica/model/Usuario.java
+++ src/main/java/com/proyecto/practica/model/Usuario.java
@@ -1,5 +1,31 @@
 package com.proyecto.practica.model;
 
-public class Usuario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false)
+	private long id;
+	@Column(name = "username", nullable = false)
+	private String username;
+	@Column(name = "password", nullable = false)
+	private String password;
+	
 }
